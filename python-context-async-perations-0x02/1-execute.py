7257638c39013894026befcd06c7277c059fe50c
Nebyou Damtew <nebyoudamtew18@gmail.com>
2025-07-13 07:23:42 +0300

    Completed context and async operations tasks

---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+class ExecuteQuery:
+    def __init__(self, query, params=()):
+        self.query = query
+        self.params = params
+
+    def __enter__(self):
+        self.conn = sqlite3.connect("users.db")
+        self.cursor = self.conn.cursor()
+        self.cursor.execute(self.query, self.params)
+        return self.cursor.fetchall()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.conn.close()
+
+# Usage
+query = "SELECT * FROM users WHERE age > ?"
+params = (25,)
+
+with ExecuteQuery(query, params) as results:
+    print(results)
---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,23 @@
+import aiosqlite
+import asyncio
+
+async def async_fetch_users():
+    async with aiosqlite.connect("users.db") as db:
+        async with db.execute("SELECT * FROM users") as cursor:
+            return await cursor.fetchall()
+
+async def async_fetch_older_users():
+    async with aiosqlite.connect("users.db") as db:
+        async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
+            return await cursor.fetchall()
+
+async def fetch_concurrently():
+    all_users, older_users = await asyncio.gather(
+        async_fetch_users(),
+        async_fetch_older_users()
+    )
+    print("All Users:", all_users)
+    print("Older Users (age > 40):", older_users)
+
+# Run async
+asyncio.run(fetch_concurrently())
