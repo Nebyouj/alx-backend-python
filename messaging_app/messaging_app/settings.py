dcf86dd1e52e03cb555d87fdcb373c137ce842c7
Nebyou Damtew <nebyoudamtew18@gmail.com>
2025-07-27 08:29:26 +0300

    Authentication and permissions

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -94,7 +94,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'messaging_app.chats.pagination.CustomPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
 }
 
